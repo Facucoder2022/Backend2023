ac04f69d14bb0146437b2aa6cf70c6cf57f19b09
Facundo Manta <facumanta10@gmail.com>
2023-03-27 21:25:25 -0300

    desafio 2

--- desafiodos/ProductManager.js
+++ desafiodos/ProductManager.js
@@ -1,21 +1,58 @@
 import ManagerProductos from "./managers/ManagerProductos.js";
 import ManagerProductos from "./managers/ManagerProductos.js";
 
-const manager = new ManagerProductos();
-const env = async() =>{
-    let primeraConsultaProductos = await manager.consultarProductos();
-    console.log(primeraConsultaProductos);
-    let user = {
-		tilte: "Buzo",
-		description: "buzo de chicago bulls",
-		price: 2500,
-		thumbnail: "Sin imagen",
-		code: 1,
-        stock: 10
+let productos = []
+
+class ProductManager {
+    constructor (){
+        this.products=productos
     }
-    let result = await manager.crearProducto(user);
-    console.log(result);
-    let segundaConsultaProductos = await manager.consultarProductos();
-    console.log(segundaConsultaProductos);
+
+    addProduct(newProduct){
+
+        if (!newProduct.title ||
+            !newProduct.description ||
+            !newProduct.price ||
+            !newProduct.thumbnail ||
+            !newProduct.code ||
+            !newProduct.stock ) return 'todos los campos son necesarios'
+
+        let product = this.products.find(prod => prod.code === newProduct.code)
+        if(product) return 'Un producto con este code ya fue ingresado'
+
+        return this.products.push({id: this.products.length+1, ... newProduct})
+    }
+    getProducts(){
+        return this.product
+    }
+    getProductById(id){
+        let product = this.products.find(prod => prod.id === id)
+        if (!product) return 'Product Not Found'
+        return product
+    }
+
+    updateProduct(){}
+    deleteProduct(){
+        let product = this.products.find(prod => prod.id === id)
+    }
 }
-env();
+const product = new ProductManager()
+
+product.addProduct({
+    title: 'Producto 1',
+    description: 'Sin descripcion',
+    price: 1800,
+    thumbnail: 'Sin imagen',
+    code: 004,
+    stock: 10
+})
+product.addProduct({
+    title: 'Producto 2',
+    description: 'Sin descripcion',
+    price: 2800,
+    thumbnail: 'Sin imagen',
+    code: 005,
+    stock: 8
+})
+
+console.log(product.getProducts());
