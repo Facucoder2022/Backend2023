08bc9efceba3c9188e3d630525182352ed0c2186
Facundo Manta <facumanta10@gmail.com>
2023-05-03 20:19:45 -0300

    mas mongo

--- practicaIntegradora/src/dao/product.mongo.js
+++ practicaIntegradora/src/dao/product.mongo.js
@@ -1,44 +1,32 @@
-const { productModel } = require("../models/product.model");
+const { productModel } = require("./model/product.model")
 
 class ProductManagerMongo {
-	constructor() {
-		this.productModel = productModel;
-	}
-	async getProducts() {
-		try {
-			return await productModel.find({});
-		} catch (error) {
-			throw new Error(" ProductManagerMongo.getProducts " + error.message);
-		}
-	}
-	async getProductsById(pid) {
-		try {
-			return await productModel.findOne({ _id: pid });
-		} catch (error) {
-			throw new Error(" ProductManagerMongo.getProductsById " + error.message);
-		}
-	}
-	async addProduct(newProduct) {
-		try {
-			return await productModel.create(newProduct);
-		} catch (error) {
-			throw new Error(" ProductManagerMongo.AddProduct " + error.message);
-		}
-	}
-	async updateProduct(pid, updateBody) {
-		try {
-			return await productModel.updateOne({ _id: pid }, updateBody);
-		} catch (error) {
-			throw new Error(" ProductManagerMongo.updateProduct " + error.message);
-		}
-	}
-	async deleteProduct(pid) {
-		try {
-			return await productModel.deleteOne({ _id: pid });
-		} catch (error) {
-			throw new Error(" ProductManagerMongo.deleteProduct " + error.message);
-		}
-	}
+    
+    async getProducts(){
+        try{
+            return await productModel.find({})
+        }catch(err){
+            return new Error(err)
+        }
+    }
+    async getProductById(pid){
+        try {            
+            return await productModel.findOne({_id: pid})
+        } catch (error) {
+            return new Error(error)
+        }
+
+    }
+    async addProduct(newProduct){
+        try {
+            
+            return await productModel.create(newProduct)
+        } catch (error) {
+            return new Error(error)
+        }
+    }
+    async updateProduct(pid){}
+    async deleteProduct(pid){}
 }
 
-module.exports = ProductManagerMongo;
+module.exports = new ProductManagerMongo
