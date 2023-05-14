3b04872952e430b8587aa826b2c847a8a8582068
Facundo Manta <facumanta10@gmail.com>
2023-05-13 21:18:01 -0300

    updeteo mongo

--- practicaIntegradora/src/config/configServer.js
+++ practicaIntegradora/src/config/configServer.js
@@ -2,7 +2,7 @@ const {connect} = require('mongoose')
 
 module.exports = {
     connectDb: ()=>{
-        connect('mongodb://localhost:27017/miEcommerce')
+        connect('mongodb+srv://facumanta10:6VXFGaou1y8F4X8H@fmantabackend.tpf6egh.mongodb.net/?retryWrites=true&w=majority')
         console.log('Base de datos conectada')
     }
 }
